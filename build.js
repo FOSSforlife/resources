100827dd6750d53bd5fb42f51de501e5603eeef5
Elias Jackson <ejthedj0@gmail.com>
2021-02-24 21:24:27 -0800

    Only mkdir if doesn't exist

--- build.js
+++ build.js
@@ -4,14 +4,17 @@ const { noCase } = require('change-case');
 const yaml = require('js-yaml');
 const ejs = require('ejs');
 
+// TODO: Copy from public dir
 const template = ejs.compile(fs.readFileSync('./views/pages/entry.ejs').toString());
-for (const dataDir of fs.readdirSync(`./data`)) {
-  for(const entryFile of fs.readdirSync(`./data/${dataDir}`, {})) {
+for (const dataDir of fs.readdirSync(`data`)) {
+  for(const entryFile of fs.readdirSync(`data/${dataDir}`, {})) {
     const entryName = entryFile.split('.')[0];
-    const entryData = yaml.load(fs.readFileSync(`./data/${dataDir}/${entryFile}`));
+    const entryData = yaml.load(fs.readFileSync(`data/${dataDir}/${entryFile}`));
 
     const html = template({ entry: entryData });
-    fs.mkdirSync('./dist');
-    fs.writeFileSync(`./dist/${titleCase(noCase(entryName))}.html`, html);
+    if(!fs.existsSync('dist')) {
+      fs.mkdirSync('dist');
+    };
+    fs.writeFileSync(`dist/${titleCase(noCase(entryName))}.html`, html);
   }
 }
